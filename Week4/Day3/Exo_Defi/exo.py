27fe3f1f2e8f931b91c6aea52a598453997942ad
07gitgp01 <guigmawpaulin@gmail.com>
2023-01-08 12:12:41 +0100

    day3 new commit

--- Week4/Day3/Exo_Defi/exo.py
+++ Week4/Day3/Exo_Defi/exo.py
@@ -17,66 +17,21 @@ Exemples
 
 """ 
 ### Code ci-dessous:
-# word = input(f"Entrer un nom\nNom: ")
-# liste = []
-# ilist = []
-# dic = {}
-# # for (ind, val) in enumerate(word):
-# #     liste.append((ind, val))
-# #     dic[]
-# #     print(liste[list(ind)], liste[val])
-    
-#     #ilist.append(ind)
-#     #print(ind, val)
-# list1 = list(word)
-# rep = []
-# nt = 0
-# for i in range(len(list1)):
-#     #rep = [i]
-#     #liste=[i]
-#     #print(liste)
-#     if not (list1[i] in dic.keys()):
-#         dic[list1[i]] = [i]
-#         #print(dic)
-#         #rep = dic[list1[i]]
-#         rep = [i-(nt-list1.index(list1[i]))]
-#         #nt +=1
-#     elif (list1[i] in dic.keys()):
-#         dic[list1[i]] = rep
-#     nt +=1
-    
-#     #ilist.append(list1[i])
-# print(dic)
-
-word = input("Entrez une chaine\nChaine: ")
-mot=list(word)
-i = 0
-nb=len(mot)
-dic= {}
-while i<nb:
-    if mot[i] in dic:
-        for j in word:
-            if j==mot[i]:
-                n=word.index(j)
-                liste.append(n) 
+print("************************ DEFI1 *********************************")
+dic1 = {}
+dic2 = {}
+word = input("Entrez une chaine de caractère\nChaine: ")
+for ind, val in enumerate (word):
+    if not (val in dic1.keys()):
+        dic1[val] = [ind]
+        dic2[val] = [ind]
+    elif (val in dic1.keys() and val in dic2.keys()):
+        dic1[val] = dic2[val]+[ind]
+        dic2[val] += [ind] 
     else:
-        liste=[]
-        for j in word:
-            if j==mot[i]:
-                n=word.index(j)
-                liste.append(n)
-    dic.update({mot[i]:liste})
-    i+=1
-print(dic)
-    
+        continue
+print(dic1)
         
-
-        
-
-    
-
-
-    
 
 
 """
@@ -123,3 +78,80 @@ wallet = "$1"
 
 ➞ "Nothing"
 """
+###Code ci-dessous:
+
+
+
+
+### Defi 2
+print("********************************** DEFI 2****************************\n\n*************************** WELCOME INTO OUR CHOP *********************")
+
+items_Block1 = {
+  "Water": "1",
+  "Bread": "3",
+  "TV": "1000",
+  "Fertilizer": "20"
+}
+
+items_Block2 = {
+  "Apple": "4",
+  "Honey": "3",
+  "Fan": "14",
+  "Bananas": "4",
+  "Pan": "100",
+  "Spoon": "2"
+}
+
+items_Block3 = {
+  "Phone": "999",
+  "Speakers": "300",
+  "Laptop": "5000",
+  "PC": "1200"
+}
+
+Monwallet = input(f"Entrez la somme de votre portefeuille\nsomme: ")
+liste1 = []
+liste2 = []
+liste3 = []
+choix = 1
+while ( choix == 1 or choix == 2 or choix == 3 or choix == 0):
+    choix = int(input("\nEntrer un choix\n\t\t'1' ==> pour le block1\n\t\t'2' ==> pour lr block2\n\t\t'3' ==> pour le block3\n\t\t'0' pour quitter\nChoix: "))
+    while (choix == 1):
+        for prod, prix in items_Block1.items():
+            if (int(Monwallet) >= int(prix)):
+                liste1.append(prod)
+            else:
+                continue
+        if liste1 == []:
+            print("\nRien!")
+        else:
+            print(f"\nLa liste disponible pour vous dans Block1==>", liste1)  
+        break
+
+    while (choix == 2):
+        for prod, prix in items_Block2.items():
+            if (int(Monwallet) >= int(prix)):
+                liste2.append(prod)
+            else:
+                continue
+        if liste2 == []:
+            print("\nRien!")
+        else:
+            print(f"\nLa liste disponible pour vous dans Block1==>", liste2)
+        break
+
+    while (choix == 3):
+        for prod, prix in items_Block3.items():
+            if (int(Monwallet) >= int(prix)):
+                liste3.append(prod)
+            else:
+                continue
+        if liste3 == []:
+            print("\nRien!")
+        else:
+            print(f"\nLa liste disponible pour vous dans Block1==>", liste3)
+        break
+
+    if (choix == 0):
+        break
+
